e032062121a19e2b2ae179a74a8a98b46a96078b
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2020-08-10 21:48:45 -0400

    Add files via upload

--- WM.py
+++ WM.py
@@ -1,18 +1,19 @@
 import tkinter
 from tkinter import *
 
-window = tkinter.Tk()
-window.attributes("-fullscreen", True)
 
+def user_input():
+    user_input = tkinter.Tk()
+    txt=Entry(user_input, text="testing", bd=5)
+    txt.place(x=40, y=50)
+    mainloop()
 
-
-mainloop()
-
-
-
-
-
-
+def background():
+    window = tkinter.Tk()
+    window.attributes("-fullscreen", False)
+    mainloop()
+    user_input()
+background()
 
 
 
