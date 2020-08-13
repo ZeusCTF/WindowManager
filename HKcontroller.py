ad86b856a63a14916f63a0d1847eacf9f37f7d0e
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2020-08-13 16:43:46 -0400

    Add files via upload

---
+++ HKcontroller.py
@@ -0,0 +1,26 @@
+from pynput import keyboard
+from pynput.keyboard import Key, Listener
+from subprocess import Popen, PIPE
+from UserInput import InputWindow
+
+
+def HK_controller():
+    def on_activate():
+        x = InputWindow()
+
+    def for_canonical(f):
+        return lambda k: f(l.canonical(k))
+
+    hotkey = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+<alt>+<enter>'), on_activate)
+
+    with keyboard.Listener(on_press=for_canonical(hotkey.press),on_release=for_canonical(hotkey.release)) as l:
+        l.join()
+
+    def on_release(key):
+        if key == Key.esc:
+            return False
+    on_release()
+
+HK_controller()
+
+
