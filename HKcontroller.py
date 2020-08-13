dbcdd51f363f25dbccd176859eafb308c75c8e29
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2020-08-13 17:16:05 -0400

    Update HKcontroller.py

--- HKcontroller.py
+++ HKcontroller.py
@@ -11,7 +11,7 @@ def HK_controller():
     def for_canonical(f):
         return lambda k: f(l.canonical(k))
 
-    hotkey = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+<alt>+<enter>'), on_activate)
+    hotkey = keyboard.HotKey(keyboard.HotKey.parse('<cmd>+<enter>'), on_activate)
 
     with keyboard.Listener(on_press=for_canonical(hotkey.press),on_release=for_canonical(hotkey.release)) as l:
         l.join()
